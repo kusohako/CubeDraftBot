6edb84a0bc67f998c01bde5d540a471d1bce47b1
kusohako <kirbylove02@gmail.com>
2021-05-17 23:39:15 +0900

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CubeDraftBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
